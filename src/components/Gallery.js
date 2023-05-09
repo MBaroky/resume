498a3dad20b8429dcfad7663107c9f333c22e78c
Mohamed Gamal El Baroky <m.baroky@gmail.com>
2023-05-09 13:19:35 +0000

    minor but necessary modifies

--- src/components/Gallery.js
+++ src/components/Gallery.js
@@ -1,43 +1,40 @@
-import React from 'react';
-import Lightbox from 'bs5-lightbox'
+import React from "react";
+import Lightbox from "bs5-lightbox";
 
-function Gallery({items, type}) {
-
+function Gallery({ items, type }) {
   const onClick = e => {
     e.preventDefault();
-    const el = e.target;
+    const el = e.currentTarget;
     const lightbox = new Lightbox(el);
     lightbox.show();
-    console.log(el);
   };
-    return (
+  return (
     <div className='row photos gallery'>
-        {items.map(item => {
-            const {id, type, link, thumb} = item
-            const typeName = type
-            return (
-              <div
-                key={id}
-                className='col-sm-6 col-md-4 col-lg-3 item'>
-                <a
-                  data-toggle='lightbox'
-                  data-gallery={typeName? typeName:""}
-                  onClick={onClick}
-                  href={link}
-                  data-caption=''>
-                  <img
-                    alt='alt-text'
-                    className='img-fluid'
-                    style={{
-                      pointerEvents: "none",
-                    }}
-                    src={thumb}
-                  />
-                </a>
-              </div>
-            )})}
-      </div>
-     );
+      {items.map(item => {
+        const { id, link, thumb, desc } = item;
+        const typeName = type;
+        return (
+          <div key={id} className='col-sm-6 col-md-4 col-lg-3 item'>
+            <a
+              data-toggle='lightbox'
+              data-gallery={typeName ? typeName : ""}
+              onClick={onClick}
+              href={link}
+              data-caption={desc}>
+              <img
+                alt='alt-text'
+                className='img-fluid'
+                style={{
+                  pointerEvents: "none",
+                }}
+                src={thumb}
+              />
+            </a>
+          </div>
+        );
+      })}
+    </div>
+  );
 }
 
 export default Gallery;
